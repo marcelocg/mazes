da933decc4d9a027717bf088e9f08234626dc1dc
Marcelo Gonçalves <marcelocg@gmail.com>
2016-01-28 10:53:49 -0300

    Trying to output colored regions (Unix only)

--- Java/src/mcg/maze/Maze.java
+++ Java/src/mcg/maze/Maze.java
@@ -1,5 +1,8 @@
 package mcg.maze;
 
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,8 +18,8 @@ public class Maze {
   int sizeY;
 
   //0-None, 1-Partial Mazes only, 2-Mazes and text
-  static int DEBUGLEVEL = 1;
-  static int THRESHOLD = 4;
+  static int DEBUGLEVEL = 2;
+  static int THRESHOLD = 16;
   
   static int NORTH = 1;
   static int SOUTH = 2;
@@ -29,7 +32,37 @@ public class Maze {
 
   private ArrayList<Cell> grid;
 
-  public Maze() {
+  public static final char[] EXTENDED = { 0x00C7, 0x00FC, 0x00E9, 0x00E2,
+      0x00E4, 0x00E0, 0x00E5, 0x00E7, 0x00EA, 0x00EB, 0x00E8, 0x00EF,
+      0x00EE, 0x00EC, 0x00C4, 0x00C5, 0x00C9, 0x00E6, 0x00C6, 0x00F4,
+      0x00F6, 0x00F2, 0x00FB, 0x00F9, 0x00FF, 0x00D6, 0x00DC, 0x00A2,
+      0x00A3, 0x00A5, 0x20A7, 0x0192, 0x00E1, 0x00ED, 0x00F3, 0x00FA,
+      0x00F1, 0x00D1, 0x00AA, 0x00BA, 0x00BF, 0x2310, 0x00AC, 0x00BD,
+      0x00BC, 0x00A1, 0x00AB, 0x00BB, 0x2591, 0x2592, 0x2593, 0x2502,
+      0x2524, 0x2561, 0x2562, 0x2556, 0x2555, 0x2563, 0x2551, 0x2557,
+      0x255D, 0x255C, 0x255B, 0x2510, 0x2514, 0x2534, 0x252C, 0x251C,
+      0x2500, 0x253C, 0x255E, 0x255F, 0x255A, 0x2554, 0x2569, 0x2566,
+      0x2560, 0x2550, 0x256C, 0x2567, 0x2568, 0x2564, 0x2565, 0x2559,
+      0x2558, 0x2552, 0x2553, 0x256B, 0x256A, 0x2518, 0x250C, 0x2588,
+      0x2584, 0x258C, 0x2590, 0x2580, 0x03B1, 0x00DF, 0x0393, 0x03C0,
+      0x03A3, 0x03C3, 0x00B5, 0x03C4, 0x03A6, 0x0398, 0x03A9, 0x03B4,
+      0x221E, 0x03C6, 0x03B5, 0x2229, 0x2261, 0x00B1, 0x2265, 0x2264,
+      0x2320, 0x2321, 0x00F7, 0x2248, 0x00B0, 0x2219, 0x00B7, 0x221A,
+      0x207F, 0x00B2, 0x25A0, 0x00A0 };
+
+public static final char getAscii(int code) {
+  if (code >= 0x80 && code <= 0xFF) {
+      return EXTENDED[code - 0x7F];
+  }
+  return (char) code;
+}
+
+public static final void printChar(int code) {
+  System.out.printf("%c%n", getAscii(code));
+}
+
+
+  public Maze() throws IOException {
     this(4, 4);
   }
 
@@ -75,8 +108,8 @@ public class Maze {
     
     clearRegionMarks();
     
-    if(DEBUGLEVEL  > 1) System.out.println("Iniciando Split");
-    if(DEBUGLEVEL == 1) print();
+    if(DEBUGLEVEL >  1) System.out.println("Iniciando Split");
+    if(DEBUGLEVEL >= 1) { print(); pause(); }
     
     
     Cell seedA = getRandomUnsplitCell(region);
@@ -107,14 +140,14 @@ public class Maze {
       unsplitCellsCount = region.stream().filter(c -> !c.isInRegion()).count();
     }
     
-    if(DEBUGLEVEL  > 1) System.out.println("Split concluido.");
-    if(DEBUGLEVEL == 1) print();
-    if(DEBUGLEVEL  > 1) System.out.println("Vou construir a fronteira entre as regioes...");
+    if(DEBUGLEVEL >  1) System.out.println("Split concluido.");
+    if(DEBUGLEVEL >= 1) { print(); pause(); }
+    if(DEBUGLEVEL >  1) System.out.println("Vou construir a fronteira entre as regioes...");
     
     buildBorder(region);
 
-    if(DEBUGLEVEL  > 1) System.out.println("Fronteira construida e uma parede retirada.");
-    if(DEBUGLEVEL == 1) print();
+    if(DEBUGLEVEL >  1) System.out.println("Fronteira construida e uma parede retirada.");
+    if(DEBUGLEVEL >= 1) { print(); pause(); }
     
     List<Cell> subRegionA = region.stream().filter(c -> c.getRegion() == A).collect(Collectors.toList()); 
     List<Cell> subRegionB = region.stream().filter(c -> c.getRegion() == B).collect(Collectors.toList());
@@ -209,7 +242,7 @@ public class Maze {
       for (int x = 0;  x < sizeX; x++) {
         Cell cell = this.getCell(x, y);
 
-        string += " "+ cell.getRegionName() +" ";
+        string += " "+ cell.getRegionPic() +" ";
         string += cell.wallEast ? "|" : " ";
 
         bottom += cell.wallSouth ? "---" : "   ";
@@ -259,6 +292,17 @@ public class Maze {
     public String getRegionName() {
       String[] regNames = {" ", "A", "B"};
       return regNames[region+1];
+    }
+
+    public String getRegionPic() {
+      String RESET = (char)27 + "[0m";
+      String RED = (char)27 + "[31m";
+      String BLUE = (char)27 + "[34m";
+      
+      System.out.println(RED + "Texto vermelho" + RESET);
+      
+      String[] pics = {RESET + getAscii(32), RED + getAscii(176) + RESET, BLUE + getAscii(177) + RESET};
+      return ""+pics[region+1];
     }
     
     public boolean isInRegion() {
@@ -389,7 +433,19 @@ public class Maze {
 
   }
 
+  private void pause() {
+    try {
+      System.in.read(new byte[2]);
+    } catch (IOException e) {
+    }
+    
+    
+  
+    
+  }
+
   public static void main(String[] args) {
+    //Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
     Maze maze = new Maze(30,30);
     maze.print();
   }
